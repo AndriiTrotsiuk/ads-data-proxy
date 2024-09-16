e681b2455a65b8cc2c000d454d54301d646fce41
AndriiTrotsiuk <138963507+AndriiTrotsiuk@users.noreply.github.com>
2024-09-16 22:49:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,38 @@
 <?php
 $targetUrl = 'http://endlessdeposits.art/external/google-ads/update-stats';
 
-$parameters = $_GET;
+// Collect all the parameters from the current request's URI
+$parameters = $_GET; // Using $_GET to handle query parameters
 
-$jsonString = json_encode($parameters);
+// Create a query string from the parameters
+$queryString = http_build_query($parameters);
 
-if (json_last_error() !== JSON_ERROR_NONE) {
-    echo 'JSON Encoding Error: ' . json_last_error_msg();
-    exit;
-}
-
+// Prepare the cURL session
 $ch = curl_init();
 
+// Set the target URL (without the query string for PUT requests)
 curl_setopt($ch, CURLOPT_URL, $targetUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonString);
 
+// Set options to return the response instead of outputting it directly
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Set the HTTP method to PUT
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+
+// Set the data to be sent in the body of the PUT request
+curl_setopt($ch, CURLOPT_POSTFIELDS, $queryString);
+
+// Execute the cURL session
 $response = curl_exec($ch);
 
+// Check for errors
 if ($response === false) {
     echo 'cURL Error: ' . curl_error($ch);
 }
 
+// Close the cURL session
 curl_close($ch);
 
+// Output the response from the target server
 echo $response;
 ?>
